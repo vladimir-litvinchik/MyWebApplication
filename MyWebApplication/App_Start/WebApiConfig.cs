0303dba9724ae8db4048697e64b4dcb22745456a
Volodymyr Lytvynchyk <vlytvynchyk@gmail.com>
2021-06-02 21:28:14 +0300

    Add project files.

---
+++ MyWebApplication/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+
+namespace MyWebApplication
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Use camel case for JSON data.
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver 
+                = new CamelCasePropertyNamesContractResolver();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
